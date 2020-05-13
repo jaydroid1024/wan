9eae87bc260a57961e5c1b013310ef8bea2d612c
Jay-Droid <jay_droid@163.com>
2020-05-13 14:26:58 +0800

    gradle file update

--- biz_components/setting/src/main/java/com/jaydroid/setting/SettingActivity.kt
+++ biz_components/setting/src/main/java/com/jaydroid/setting/SettingActivity.kt
@@ -2,12 +2,17 @@ package com.jaydroid.setting
 
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import butterknife.BindView
+import butterknife.OnClick
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.jaydroid.base_component.arouter.ARHelper
 import com.jaydroid.base_component.base.mvp.BaseMVPActivity
 import com.jaydroid.base_component.network.auth.isCookieNotEmpty
 import com.jaydroid.base_component.network.bean.wan.LoggedInEvent
+import com.jaydroid.base_lib.utils.ToastUtils
 import com.jaydroid.setting.contract.SettingContract
 import com.jaydroid.setting.presenter.SettingPresenter
 import org.greenrobot.eventbus.EventBus
@@ -19,6 +24,25 @@ class SettingActivity : BaseMVPActivity<SettingContract.View, SettingPresenter>(
     private lateinit var logoutBtn: Button
 
     private lateinit var toolbar: Toolbar
+
+    @BindView(R2.id.iv_test)
+    lateinit var ivTest: ImageView
+
+    @BindView(R2.id.tv_test)
+    lateinit var tvTest: TextView
+
+    @OnClick(R2.id.iv_test, R2.id.tv_test)
+    fun onClick(view: View) {
+
+        when (view.id) {
+            R.id.iv_test -> {
+                ToastUtils.showLong("测试iv_test")
+            }
+            R.id.tv_test -> {
+                ToastUtils.showLong("测试tv_test")
+            }
+        }
+    }
 
     override fun getLayoutResId(): Int {
         return R.layout.activity_setting
@@ -36,6 +60,9 @@ class SettingActivity : BaseMVPActivity<SettingContract.View, SettingPresenter>(
         toolbar.setNavigationOnClickListener { finish() }
         logoutBtn = findViewById(R.id.btn_logout)
         logoutBtn.setOnClickListener { presenter.logout() }
+
+        ivTest.setImageResource(R.mipmap.base_component_ic_launcher)
+        tvTest.setText("测试文本")
     }
 
     override fun initData() {
@@ -58,4 +85,6 @@ class SettingActivity : BaseMVPActivity<SettingContract.View, SettingPresenter>(
         EventBus.getDefault().post(LoggedInEvent(null))
         finish()
     }
+
+
 }
