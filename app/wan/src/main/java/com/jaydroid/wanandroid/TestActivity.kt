edca8ae214639bf38f7486d111ffc8d23746c831
jay <jay_droid@163.com>
2020-06-17 22:50:18 +0800

    组件化基础Demo

--- app/wan/src/main/java/com/jaydroid/wanandroid/TestActivity.kt
+++ app/wan/src/main/java/com/jaydroid/wanandroid/TestActivity.kt
@@ -10,6 +10,6 @@ class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wan_test)
+        setContentView(R.layout.activity_test)
     }
 }
